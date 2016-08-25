8473a9dee8c8a01042c8cf99c8a461cbe999758e
James Davis <james.jpdavis@gmail.com>
2016-08-25 16:16:40 -0400

    bootstrap angular cdn

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express 	= require('express');
 var bodyParser 	= require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
 app.listen(PORT, function(){
 	console.log('App listening on PORT ' + PORT);
 });
